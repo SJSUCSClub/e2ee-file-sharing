497b12be216642b59d4dfb3faa353bb25753939b
rtk0c <mail.tianyig@gmail.com>
2025-03-10 16:42:20 -0700

    cli commands skeleton

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Cli {
+    /// Username to authenticate with.
+    #[arg(short, long)]
+    name: String,
+    /// Password to authenticate with.
+    /// If unspecified, interactively prompts for the password.
+    #[arg(short, long)]
+    password: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<Subcommands>,
+}
+
+#[derive(Subcommand)]
+enum Subcommands {
+    /// Register a new account with the provided username and password credentials.
+    Register {},
+    /// Send a file to a list of recipients.
+    Send {
+        recipients: Vec<String>,
+        file: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
