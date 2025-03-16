8fb3d6528bc77001cb82514686282e72788f66cd
DhirajB-123 <dhirajbomma@gmail.com>
2025-03-15 19:03:08 -0700

    updated schema

--- server/src/db.rs
+++ server/src/db.rs
@@ -10,22 +10,20 @@ pub fn init_db() -> Result<()> {
         pk_pub BLOB NOT NULL
     );
     CREATE TABLE IF NOT EXISTS groups (
-        id INTEGER NOT NULL,
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+    ); 
+    CREATE TABLE IF NOT EXISTS groups_user_junction (
+        group_id INTEGER NOT NULL REFERENCES groups(id),
         user_id INTEGER NOT NULL REFERENCES users(id),
+        name TEXT NOT NULL,
         encrypted_key BLOB NOT NULL,
-        PRIMARY KEY (id, user_id)
+        PRIMARY KEY (group_id, user_id)
     );
     CREATE TABLE IF NOT EXISTS files (
         group_id INTEGER NOT NULL REFERENCES groups(id),
         file_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         filename TEXT NOT NULL,
         path TEXT NOT NULL
-    );
-    CREATE TABLE IF NOT EXISTS group_names (
-        group_id INTEGER NOT NULL REFERENCES groups(id),
-        user_id INTEGER NOT NULL REFERENCES users(id),
-        group_name TEXT NOT NULL,
-        PRIMARY KEY (group_id, user_id)
     );";
     conn.execute_batch(sql)
 }
